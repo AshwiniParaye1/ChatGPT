bd1b142667713523161bc2cb5f0f2d175966d01b
Ashwini <ashwiniparaye9309@gmail.com>
2023-05-19 18:42:38 +0530

    api fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,13 @@
 export const fetchResponse = async (chat) => {
   try {
+    // after depoloyment you should change the fetch URL below
     const response = await fetch("http://localhost:3080", {
       method: "POST",
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        message: chat.map((message) => message.message.join(" \n")),
+        message: chat.map((message) => message.message).join(" \n "),
       }),
     });
 
